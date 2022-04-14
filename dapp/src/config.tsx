4da6d2a06e599850b98b5294a62b22a2c15bbf99
mobo <morgan.bohn@pc-scol.fr>
2022-04-14 14:41:24 +0200

    [SCOL-15360] deadrare and trustmarket link

--- dapp/src/config.tsx
+++ dapp/src/config.tsx
@@ -2,3 +2,9 @@ export const contractAddress =
   'erd1qqqqqqqqqqqqqpgqzau4u3scnqtthpu7p6fjp0fpwvpcagqtys5sezss7w';
 
 export const dAppName = 'DeadBrothers Club Dashboard';
+
+export const elrondApiUrl = 'https://api.elrond.com';
+export const nftsCollectionId = 'DEADBROS-bf822f';
+export const deadTokenId = 'DEADBROS-388e63';
+export const deadRareUrl = 'https://deadrare.io';
+export const trustMarketUrl = 'https://trust.market';
--- dapp/src/pages/Dashboard/DeadBros.tsx
+++ dapp/src/pages/Dashboard/DeadBros.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import { useGetAccountInfo, DappUI } from '@elrondnetwork/dapp-core';
-import { contractAddress } from 'config';
+import {
+  contractAddress,
+  deadRareUrl,
+  deadTokenId,
+  elrondApiUrl,
+  nftsCollectionId,
+  trustMarketUrl
+} from 'config';
 import axios from 'axios';
 
 import {
   faArrowUp,
   faCheck,
-  faGamepad
+  faGamepad,
+  faBolt,
+  faShoppingCart,
+  faSkull
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
@@ -35,9 +45,7 @@ const DeadBros = () => {
     // Use [] as second argument in useEffect for not rendering each time
     axios
       .get<any>(
-        'https://api.elrond.com/accounts/' +
-          address +
-          '/nfts?collections=DEADBROS-bf822f'
+        `${elrondApiUrl}/accounts/${address}/nfts?collections=${nftsCollectionId}`
       )
       .then((response) => {
         setBrosList(response.data);
@@ -47,25 +55,11 @@ const DeadBros = () => {
   React.useEffect(() => {
     // Use [] as second argument in useEffect for not rendering each time
     axios
-      .get<any>(
-        'https://api.elrond.com/accounts/' + address + '/tokens/DEADBROS-388e63'
-      )
+      .get<any>(`${elrondApiUrl}/accounts/${address}/tokens/${deadTokenId}`)
       .then((response) => {
         setDeadToken(response.data);
       });
   }, []);
-
-  const staking = () => {
-    alert('STAKING WIP ...');
-  };
-
-  const vote = () => {
-    alert('VOTE WIP ...');
-  };
-
-  const play = () => {
-    alert('PLAY WIP ...');
-  };
 
   const getAttributes = (bro: Bro): Array<string> => {
     return bro.metadata?.attributes?.map(
@@ -81,7 +75,7 @@ const DeadBros = () => {
 
   return (
     <div className='col mt-4 col-md-12'>
-      <h3>My tokens</h3>
+      <h3>$DEAD Token</h3>
       <div className='row'>
         <div className='col'>
           {dead === undefined && (
@@ -95,7 +89,9 @@ const DeadBros = () => {
         </div>
       </div>
       <hr />
-      <h3>My cimetary</h3>
+      <h3>
+        #DeadBrothers <FontAwesomeIcon icon={faSkull} className='text' />
+      </h3>
       <div className='row'>
         {bros !== undefined && bros.length === 0 && (
           <div className='col'>
@@ -105,7 +101,10 @@ const DeadBros = () => {
         {bros !== undefined &&
           bros.length > 0 &&
           bros.map((bro) => (
-            <div key={bro.identifier} className='col mt-4 mx-auto'>
+            <div
+              key={bro.identifier}
+              className='col-12 col-sm-12 col-md-6 col-lg-4 mt-4 mx-auto'
+            >
               <div>
                 <b>{bro.name}</b>
               </div>
@@ -125,19 +124,31 @@ const DeadBros = () => {
                   </OverlayTrigger>
                 </div>
               </div>
-              <div className='mt-2'>
-                <button className='btn-primary' onClick={staking}>
-                  STAKE&nbsp;
-                  <FontAwesomeIcon icon={faArrowUp} className='text' />
-                </button>
-                <button className='btn-primary ml-1' onClick={vote}>
-                  VOTE&nbsp;
-                  <FontAwesomeIcon icon={faCheck} className='text' />
-                </button>
-                <button className='btn-primary ml-1' onClick={play}>
-                  PLAY&nbsp;
-                  <FontAwesomeIcon icon={faGamepad} className='text' />
-                </button>
+              <div>
+                <div className='w-100'></div>
+                <a
+                  className='btn btn-primary ml-1 mt-2'
+                  role='button'
+                  aria-pressed='true'
+                  href={deadRareUrl + '/nft/' + bro.identifier}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  DEADRARE&nbsp;
+                  <FontAwesomeIcon icon={faBolt} className='text' />
+                </a>
+                <div className='w-100'></div>
+                <a
+                  className='btn btn-primary ml-1 mt-2'
+                  role='button'
+                  aria-pressed='true'
+                  href={trustMarketUrl + '/nft/' + bro.identifier}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  TRUSTMARKET&nbsp;
+                  <FontAwesomeIcon icon={faShoppingCart} className='text' />
+                </a>
               </div>
             </div>
           ))}
