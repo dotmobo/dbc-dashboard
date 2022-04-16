763ac28c50e351588b1d2676a6c3c41390f72f69
mobo <morgan.bohn@pc-scol.fr>
2022-04-16 19:21:29 +0200

    fix color

--- dapp/src/pages/Dashboard/TopInfo.tsx
+++ dapp/src/pages/Dashboard/TopInfo.tsx
@@ -8,7 +8,7 @@ const TopInfo = () => {
   return (
     <div className='text-white' data-testid='topInfo'>
       <div className='mb-1'>
-        <span className='opacity-6 mr-1'>Your address:</span>
+        <span className='mr-1'>Your address:</span>
         <span data-testid='accountAddress'> {address}</span>
       </div>
       {/* <div className='mb-4'>
