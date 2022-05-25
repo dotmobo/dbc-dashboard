0ada84dc28761fdeb00c0fd5d606e5fcb5c59de9
mobo <morgan.bohn@pc-scol.fr>
2022-05-25 18:31:37 +0200

    wip staking 4

--- dapp/src/pages/Stake/Components/NftStaking.tsx
+++ dapp/src/pages/Stake/Components/NftStaking.tsx
@@ -414,7 +414,7 @@ const NftStaking = () => {
           stakeds.map((bro) => (
             <div
               key={bro.identifier}
-              className='col-12 col-sm-12 col-md-12 col-lg-12 mt-4 mx-auto'
+              className='col-12 col-sm-12 col-md-12 col-lg-12 mt-3 mx-auto'
             >
               <LazyLoad height={200} offset={100} once>
                 <div>
@@ -512,7 +512,7 @@ const NftStaking = () => {
           bros.map((bro) => (
             <div
               key={bro.identifier}
-              className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 mx-auto'
+              className='col-6 col-sm-6 col-md-4 col-lg-3 mt-3 mx-auto'
             >
               <LazyLoad height={200} offset={100} once>
                 <div>
