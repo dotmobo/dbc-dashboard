4d113a49691d5ee2e0e496b91419a750c4534e1e
mobo <morgan.bohn@pc-scol.fr>
2022-05-31 17:32:20 +0200

    add genesis floor price 3

--- dapp/src/pages/Info/Components/Stats.tsx
+++ dapp/src/pages/Info/Components/Stats.tsx
@@ -12,6 +12,9 @@ const Stats = () => {
       </h3>
       <div className='row'>
         <div className='col'>
+          <div>
+            <small className='text-muted'>GENESIS</small>
+          </div>
           <iframe width='100%' height='660' src={omniscientUrl}></iframe>
         </div>
       </div>
