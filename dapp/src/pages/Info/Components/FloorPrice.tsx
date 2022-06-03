b67738838cf3b72d67ae360705e6e1d0217b612a
mobo <morgan.bohn@pc-scol.fr>
2022-06-03 14:50:20 +0200

    legendary and danw floor price

--- dapp/src/pages/Info/Components/FloorPrice.tsx
+++ dapp/src/pages/Info/Components/FloorPrice.tsx
@@ -9,7 +9,9 @@ import {
   trustMarketUrl,
   gatewayFrameIt,
   frameItIconUrl,
-  frameItUrl
+  frameItUrl,
+  nftsLegendaryCollectionId,
+  nftsDawnCollectionId
 } from 'config';
 import axios from 'axios';
 import {
@@ -29,6 +31,18 @@ const FloorPrice = () => {
   const [floorPriceTR, setFloorPriceTR] = React.useState<FloorPrice>();
   const [floorPriceFI, setFloorPriceFI] = React.useState<FloorPrice>();
 
+  const [floorPriceLegendaryDR, setFloorPriceLegendaryDR] =
+    React.useState<FloorPrice>();
+  const [floorPriceLegendaryTR, setFloorPriceLegendaryTR] =
+    React.useState<FloorPrice>();
+  const [floorPriceLegendaryFI, setFloorPriceLegendaryFI] =
+    React.useState<FloorPrice>();
+
+  const [floorPriceDawnDR, setFloorPriceDawnDR] = React.useState<FloorPrice>();
+  const [floorPriceDawnTR, setFloorPriceDawnTR] = React.useState<FloorPrice>();
+  const [floorPriceDawnFI, setFloorPriceDawnFI] = React.useState<FloorPrice>();
+
+  // GENESIS
   React.useEffect(() => {
     // Use [] as second argument in useEffect for not rendering each time
     axios({
@@ -60,6 +74,82 @@ const FloorPrice = () => {
         floorPrice: response.data.statistics.floorPrice
       } as FloorPrice);
     });
+  }, []);
+
+  // LEGENDARY
+  React.useEffect(() => {
+    // Use [] as second argument in useEffect for not rendering each time
+    axios({
+      url: `${gatewayDeadRareUrl}`,
+      method: 'post',
+      data: {
+        query: `{floorPrice(collection: "${nftsLegendaryCollectionId}")}`
+      }
+    }).then((response) => {
+      setFloorPriceLegendaryDR(response.data.data);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    // Use [] as second argument in useEffect for not rendering each time
+    axios
+      .get<any>(
+        `${gatewayTrustMarket}/getFloorPrice/${nftsLegendaryCollectionId}/EGLD`
+      )
+      .then((response) => {
+        setFloorPriceLegendaryTR({
+          floorPrice: response.data
+        } as FloorPrice);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    // Use [] as second argument in useEffect for not rendering each time
+    axios
+      .get<any>(`${gatewayFrameIt}/${nftsLegendaryCollectionId}`)
+      .then((response) => {
+        setFloorPriceLegendaryFI({
+          floorPrice: response.data.statistics.floorPrice
+        } as FloorPrice);
+      });
+  }, []);
+
+  // DAWN
+  React.useEffect(() => {
+    // Use [] as second argument in useEffect for not rendering each time
+    axios({
+      url: `${gatewayDeadRareUrl}`,
+      method: 'post',
+      data: {
+        query: `{floorPrice(collection: "${nftsDawnCollectionId}")}`
+      }
+    }).then((response) => {
+      setFloorPriceDawnDR(response.data.data);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    // Use [] as second argument in useEffect for not rendering each time
+    axios
+      .get<any>(
+        `${gatewayTrustMarket}/getFloorPrice/${nftsDawnCollectionId}/EGLD`
+      )
+      .then((response) => {
+        setFloorPriceDawnTR({
+          floorPrice: response.data
+        } as FloorPrice);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    // Use [] as second argument in useEffect for not rendering each time
+    axios
+      .get<any>(`${gatewayFrameIt}/${nftsDawnCollectionId}`)
+      .then((response) => {
+        setFloorPriceDawnFI({
+          floorPrice: response.data.statistics.floorPrice
+        } as FloorPrice);
+      });
   }, []);
 
   return (
@@ -148,6 +238,176 @@ const FloorPrice = () => {
               </a>
             </div>
           )}
+          <div className='mt-3'>
+            <small className='text-muted'>LEGENDARY</small>
+          </div>
+          {floorPriceLegendaryDR === undefined && (
+            <div>
+              <div className='spinner-border text-primary mr-2' role='status'>
+                <span className='sr-only'>Loading...</span>
+              </div>
+            </div>
+          )}
+          {floorPriceLegendaryDR !== undefined &&
+            floorPriceLegendaryDR.floorPrice !== undefined &&
+            floorPriceLegendaryDR.floorPrice !== null && (
+              <div>
+                <img src={deadRareIconUrl} alt='deadrare' height={16} />
+                <b>DeadRare</b>:&nbsp;
+                <span>{floorPriceLegendaryDR?.floorPrice}&nbsp;EGLD</span>
+                <a
+                  className='btn btn-primary ml-3 btn-sm'
+                  role='button'
+                  aria-pressed='true'
+                  href={
+                    deadRareUrl + '/collection/' + nftsLegendaryCollectionId
+                  }
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  BUY&nbsp;
+                  <FontAwesomeIcon icon={faBolt} className='text' />
+                </a>
+              </div>
+            )}
+          {floorPriceLegendaryTR === undefined && (
+            <div>
+              <div className='spinner-border text-primary mr-2' role='status'>
+                <span className='sr-only'>Loading...</span>
+              </div>
+            </div>
+          )}
+          {floorPriceLegendaryTR !== undefined &&
+            floorPriceLegendaryTR.floorPrice !== undefined && (
+              <div>
+                <img src={trustMarketIconUrl} alt='trustmarket' height={16} />
+                <b>TrustMarket</b>:&nbsp;
+                <span>{floorPriceLegendaryTR?.floorPrice}&nbsp;EGLD</span>
+                <a
+                  className='btn btn-primary ml-3 mt-1 btn-sm'
+                  role='button'
+                  aria-pressed='true'
+                  href={
+                    trustMarketUrl + '/collection/' + nftsLegendaryCollectionId
+                  }
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  BUY&nbsp;
+                  <FontAwesomeIcon icon={faShoppingCart} className='text' />
+                </a>
+              </div>
+            )}
+          {floorPriceLegendaryFI === undefined && (
+            <div>
+              <div className='spinner-border text-primary mr-2' role='status'>
+                <span className='sr-only'>Loading...</span>
+              </div>
+            </div>
+          )}
+          {floorPriceLegendaryFI !== undefined &&
+            floorPriceLegendaryFI.floorPrice !== undefined && (
+              <div>
+                <img src={frameItIconUrl} alt='frameit' height={16} />
+                <b>FrameIt</b>:&nbsp;
+                <span>{floorPriceLegendaryFI?.floorPrice}&nbsp;EGLD</span>
+                <a
+                  className='btn btn-primary ml-3 mt-1 btn-sm'
+                  role='button'
+                  aria-pressed='true'
+                  href={
+                    frameItUrl + '/marketplace/' + nftsLegendaryCollectionId
+                  }
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  BUY&nbsp;
+                  <FontAwesomeIcon icon={faCropSimple} className='text' />
+                </a>
+              </div>
+            )}
+          <div className='mt-3'>
+            <small className='text-muted'>DAWN</small>
+          </div>
+          {floorPriceDawnDR === undefined && (
+            <div>
+              <div className='spinner-border text-primary mr-2' role='status'>
+                <span className='sr-only'>Loading...</span>
+              </div>
+            </div>
+          )}
+          {floorPriceDawnDR !== undefined &&
+            floorPriceDawnDR.floorPrice !== undefined &&
+            floorPriceDawnDR.floorPrice !== null && (
+              <div>
+                <img src={deadRareIconUrl} alt='deadrare' height={16} />
+                <b>DeadRare</b>:&nbsp;
+                <span>{floorPriceLegendaryDR?.floorPrice}&nbsp;EGLD</span>
+                <a
+                  className='btn btn-primary ml-3 btn-sm'
+                  role='button'
+                  aria-pressed='true'
+                  href={deadRareUrl + '/collection/' + nftsDawnCollectionId}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  BUY&nbsp;
+                  <FontAwesomeIcon icon={faBolt} className='text' />
+                </a>
+              </div>
+            )}
+          {floorPriceDawnTR === undefined && (
+            <div>
+              <div className='spinner-border text-primary mr-2' role='status'>
+                <span className='sr-only'>Loading...</span>
+              </div>
+            </div>
+          )}
+          {floorPriceDawnTR !== undefined &&
+            floorPriceDawnTR.floorPrice !== undefined && (
+              <div>
+                <img src={trustMarketIconUrl} alt='trustmarket' height={16} />
+                <b>TrustMarket</b>:&nbsp;
+                <span>{floorPriceDawnTR?.floorPrice}&nbsp;EGLD</span>
+                <a
+                  className='btn btn-primary ml-3 mt-1 btn-sm'
+                  role='button'
+                  aria-pressed='true'
+                  href={trustMarketUrl + '/collection/' + nftsDawnCollectionId}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  BUY&nbsp;
+                  <FontAwesomeIcon icon={faShoppingCart} className='text' />
+                </a>
+              </div>
+            )}
+          {floorPriceDawnFI === undefined && (
+            <div>
+              <div className='spinner-border text-primary mr-2' role='status'>
+                <span className='sr-only'>Loading...</span>
+              </div>
+            </div>
+          )}
+          {floorPriceDawnFI !== undefined &&
+            floorPriceDawnFI.floorPrice !== undefined && (
+              <div>
+                <img src={frameItIconUrl} alt='frameit' height={16} />
+                <b>FrameIt</b>:&nbsp;
+                <span>{floorPriceDawnFI?.floorPrice}&nbsp;EGLD</span>
+                <a
+                  className='btn btn-primary ml-3 mt-1 btn-sm'
+                  role='button'
+                  aria-pressed='true'
+                  href={frameItUrl + '/marketplace/' + nftsDawnCollectionId}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  BUY&nbsp;
+                  <FontAwesomeIcon icon={faCropSimple} className='text' />
+                </a>
+              </div>
+            )}
         </div>
       </div>
     </div>
