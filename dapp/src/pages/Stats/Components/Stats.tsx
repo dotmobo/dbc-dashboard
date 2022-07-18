fb926a446f556141fda9a7d88883053a0d44f7a0
Morgan <morgan.bohn@pc-scol.fr>
2022-07-18 16:17:27 +0200

    improve stats

--- dapp/src/pages/Stats/Components/Stats.tsx
+++ dapp/src/pages/Stats/Components/Stats.tsx
@@ -1,29 +1,14 @@
 import * as React from 'react';
-import { omniscientUrl1, omniscientUrl2 } from 'config';
-import { faChartBar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Stats = () => {
+const Stats = ({ title, url }: any) => {
   return (
     <div>
-      <h3>
-        Stats&nbsp;
-        <FontAwesomeIcon icon={faChartBar} className='text' />
-      </h3>
       <div className='row'>
         <div className='col'>
           <div>
-            <small className='text-muted'>GENESIS</small>
+            <small className='text-muted'>{title}</small>
           </div>
-          <iframe width='100%' height='550' src={omniscientUrl1}></iframe>
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <div>
-            <small className='text-muted'>DAWN</small>
-          </div>
-          <iframe width='100%' height='550' src={omniscientUrl2}></iframe>
+          <iframe width='100%' height='550' src={url}></iframe>
         </div>
       </div>
     </div>
--- dapp/src/pages/Stats/Components/index.tsx
+++ dapp/src/pages/Stats/Components/index.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
 import Stats from './Stats';
+import { faChartBar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  nftsCollectionMintName,
+  nftsDawnCollectionMintName,
+  omniscientUrl1,
+  omniscientUrl2
+} from 'config';
 
 const Components = () => {
   return (
     <div className='col mt-4 col-md-12'>
       <hr />
-      <Stats />
+      <h3>
+        Stats&nbsp;
+        <FontAwesomeIcon icon={faChartBar} className='text' />
+      </h3>
+      {!!omniscientUrl1 && (
+        <Stats title={nftsCollectionMintName} url={omniscientUrl1} />
+      )}
+      {!!omniscientUrl2 && (
+        <Stats title={nftsDawnCollectionMintName} url={omniscientUrl2} />
+      )}
     </div>
   );
 };
